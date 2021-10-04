ed164b8a1f4a398bbbc24968c9013d64101848bf
ksiar <konradsiar@gmail.com>
2021-10-04 13:57:19 +0200

    Init project

---
+++ backend/src/main/java/com/siarkowski/gatekeeper/server/GatekeeperServerApplication.java
@@ -0,0 +1,13 @@
+package com.siarkowski.gatekeeper.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatekeeperServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatekeeperServerApplication.class, args);
+	}
+
+}
